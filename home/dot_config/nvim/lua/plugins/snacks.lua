82ead7a18668b6d5e52ba89f4389ecb2829627fa
nsado <171737294+nsado123@users.noreply.github.com>
2025-03-26 05:07:53 -0700

    Remove home/.config/nvim/lazy-lock.json Update home/.config/nvim/lua/plugins/snacks.lua Update home/.config/nvim/lua/plugins/yazi.lua

--- home/dot_config/nvim/lua/plugins/snacks.lua
+++ home/dot_config/nvim/lua/plugins/snacks.lua
@@ -5,7 +5,18 @@ return {
     lazy = false,
     opts = {
       bigfile = { enabled = true },
-      dashboard = { enabled = true },
+      dashboard = { 
+        enabled = true,
+        preset = {
+          sections = {
+            { section = "header" },
+            { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+            { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+            { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+            { section = "startup" },
+          },
+        }
+      },
       explorer = { enabled = true },
       indent = { enabled = true },
       input = { enabled = true },
