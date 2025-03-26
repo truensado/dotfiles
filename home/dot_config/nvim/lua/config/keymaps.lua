f18c6fa02bd739ba1487ee414837478f6bed8d69
nsado <171737294+nsado123@users.noreply.github.com>
2025-03-26 02:16:55 -0700

    Add home/.chezmoiscripts/setup/secrets.sh Remove home/.config/fish/conf.d/uv.env.fish Add home/.config/fish/config.fish Add home/.config/fish/fish_variables Update home/.config/hypr/windowrules.conf Remove home/.config/nvim/.gitignore Remove home/.config/nvim/.neoconf.json Update home/.config/nvim/init.lua Add home/.config/nvim/lazy-lock.json Remove home/.config/nvim/lazyvim.json Remove home/.config/nvim/lua/config/autocmds.lua Remove home/.config/nvim/lua/config/keymaps.lua Update home/.config/nvim/lua/config/lazy.lua Update home/.config/nvim/lua/config/options.lua Add home/.config/nvim/lua/plugins/autopairs.lua Update home/.config/nvim/lua/plugins/catppuccin.lua Remove home/.config/nvim/lua/plugins/example.lua Add home/.config/nvim/lua/plugins/git.lua Add home/.config/nvim/lua/plugins/snacks.lua Add home/.config/nvim/lua/plugins/yazi.lua Remove home/.config/nvim/stylua.toml

--- home/dot_config/nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-local a = vim.api
-a.nvim_create_user_command("Q", "q!", {})
-a.nvim_create_user_command("W", "wq", {})
--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -1,5 +1,55 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- localization
 local o = vim.opt
-o.relativenumber = false
+local a = vim.api
+
+-- options
+o.number = true
+
+o.title = true
+
+o.autoindent = true
+o.smartindent = true
+
+o.hlsearch = true
+
+o.expandtab = true
+o.tabstop = 2
+o.softtabstop = 2
+o.shiftwidth = 2
+o.smarttab = true
+
+o.scrolloff = 10
+
+o.shell = "fish"
+
+o.backupskip = { "/tmp/*", "/private/tmp/*" }
+
+o.inccommand = "split"
+
+o.ignorecase = true
+o.smartcase = true
+
+o.wrap = false
+
+o.backspace = { "start", "eol", "indent" }
+
+o.path:append({ "**" })
+
+o.encoding = "utf-8"
+o.fileencoding = "utf-8"
+
+o.wildignore:append({ "*/node_modules/*" })
+
+o.splitbelow = true 
+o.splitright = true 
+
+o.splitkeep = "cursor"
+o.mouse = ""
+
+o.clipboard = "unnamedplus"
+
+o.termguicolors = true
+
+-- commands
+a.nvim_create_user_command("Q", "q!", {})
+a.nvim_create_user_command("W", "wq", {})
