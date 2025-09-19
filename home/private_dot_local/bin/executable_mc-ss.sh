8110ece3ff24530b8bf72f6ccfb2b2c170f702a1
nsado <171737294+nsado123@users.noreply.github.com>
2025-09-19 14:30:02 -0700

    added a bunch of shit idr;

---
+++ home/private_dot_local/bin/executable_mc-ss.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+mkdir -p "$HOME/Pictures/screenshots"
+
+cp -r "$INST_MC_DIR/screenshots/." "$HOME/Pictures/screenshots"
--- home/private_dot_local/share/hyda/bin/executable_hyda-events.sh
+++ home/private_dot_local/share/hyda/bin/executable_hyda-events.sh
@@ -1,41 +1,42 @@
 #!/usr/bin/env bash
 
-current_1=""
-current_2=""
+scroll_mode=""
+kbd_state=""
 
+kID=$(xinput list --id-only "xwayland-keyboard:1" 2>/dev/null)
+
+# Define event handler
 handle() {
   case "$1" in
-    activewindow*|activewindowv2*)
-      class=$(hyprctl -j activewindow | jq -r '.class' 2>/dev/null || echo "")
-      title=$(hyprctl -j activewindow | jq -r '.title' 2>/dev/null || echo "")
-      
-      # scroll method change
-      if [[ "$class" =~ ^steam_app_[0-9]+$ ]]; then
-        desired_1="no_scroll"
+    activewindow* | activewindowv2*)
+      local tag=$(hyprctl -j activewindow | jq -r '.tags? // [] | join(" ")' 2>/dev/null)
+
+      # ---- scroll method change
+      if [[ "$tag" == *game* ]]; then
+        new_scroll="no_scroll"
       else
-        desired_1="on_button_down"
-      fi
-      
-      if [ "$desired_1" != "$current_1" ]; then
-        hyprctl keyword input:scroll_method "$desired_1" >/dev/null 2>&1
-        current_1="$desired_1"
-      fi
-      
-      # prevent fighting games from reading keyboard as player 2
-      if [[ "$class" =~ ^steam_app_[0-9]+$ ]] && [[ "$title" =~ Guilty\ Gear\ -Strive- || "$title" =~ Street\ Fighter\ 6 ]]; then
-        desired_2="disable"
-      else
-        desired_2="enable"
+        new_scroll="on_button_down"
       fi
 
-      if [[ "$desired_2" != "$current_2" ]]; then
-        kbdID="$(xinput list --id-only "xwayland-keyboard:1" 2>/dev/null)"
-        xinput "$desired_2" "$kbdID" >/dev/null 2>&1
-        current_2="$desired_2"
+      if [[ "$new_scroll" != "$scroll_mode" ]]; then
+        hyprctl keyword input:scroll_method "$new_scroll" &> /dev/null
+        scroll_mode="$new_scroll"
       fi
- 
+
+      # ---- prevent fighting games from reading keyboard as player 2
+      if [[ "$tag" == *fighter* ]]; then
+        new_kbd="disable"
+      else
+        new_kbd="enable"
+      fi
+
+      if [[ "$new_kbd" != "$kbd_state" && -n "$kID" ]]; then
+        xinput "$new_kbd" "$kID" &> /dev/null
+        kbd_state="$new_kbd"
+      fi
       ;;
   esac
 }
 
-socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
+
+socat -U - UNIX-CONNECT:"$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do handle "$line"; done
