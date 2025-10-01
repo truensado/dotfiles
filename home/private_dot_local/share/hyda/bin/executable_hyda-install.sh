416d8aa7f060115cab258c7f5f0067e8e108ae5c
nsado <171737294+nsado123@users.noreply.github.com>
2025-10-01 04:29:26 -0700

    updated install tui to contain color and show installed label

--- home/private_dot_local/share/hyda/bin/executable_hyda-install.sh
+++ home/private_dot_local/share/hyda/bin/executable_hyda-install.sh
@@ -3,15 +3,30 @@
 fzf_args=(
   --multi
   --preview 'pacman -Sii {1}'
-  --preview-label='alt-p: toggle description, alt-j/k: scroll, tab: multi-select, F11: maximize'
+  --preview-label='alt-p: toggle description, alt-j/k: scroll, tab: multi-select'
   --preview-label-pos='bottom'
   --preview-window 'down:65%:wrap'
   --bind 'alt-p:toggle-preview'
   --bind 'alt-d:preview-half-page-down,alt-u:preview-half-page-up'
   --bind 'alt-k:preview-up,alt-j:preview-down'
-  --color 'pointer:green,marker:green'
+  --color 'pointer:#cba6f7,marker:#b4befe,prompt:#a6e3a1,border:#cba6f7,info:#f5e0dc,bg+:#45475a,fg+:#f5e0dc:bold'
 )
 
-pkgs=$(pacman -Slq | fzf "${fzf_args[@]}")
+installed_pkgs=$(pacman -Qq)
 
-[[ -n "$pkgs" ]] && echo "$pkgs" | tr '\n' ' ' | xargs sudo pacman -S --noconfirm
+pkgs=$(pacman -Sl | awk -v installed="$installed_pkgs" '
+BEGIN {
+  # put installed packages into a hash for O(1) lookups
+  n=split(installed, arr, "\n")
+  for (i=1; i<=n; i++) inst[arr[i]]=1
+}
+{
+  if (inst[$2]) {
+    status="\033[1;32m[installed]\033[0m"  # green
+  } else {
+    status=""
+  }
+  print $2, status
+}' | fzf --ansi "${fzf_args[@]}")
+
+[[ -n "$pkgs" ]] && echo "$pkgs" | awk '{print $1}' | tr '\n' ' ' | xargs sudo pacman -S --needed --noconfirm
