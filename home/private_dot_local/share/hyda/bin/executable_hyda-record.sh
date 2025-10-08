eff38abefb08c17e2ca7df04dda9d0eeda9e7426
truensado <171737294+truensado@users.noreply.github.com>
2025-10-07 18:57:39 -0700

    overhauled windowrules and preferences; changed btop keybind to include class change; overhauled record script

--- home/private_dot_local/share/hyda/bin/executable_hyda-record.sh
+++ home/private_dot_local/share/hyda/bin/executable_hyda-record.sh
@@ -3,28 +3,19 @@
 scrDir=$(dirname "$(realpath "$0")")
 source "$scrDir/hyda-variables.sh"
 
-srDir="${XDG_VIDEOS_DIR:-$HOME/Videos}/screenrecords"
+scr_dir="${XDG_VIDEOS_DIR:-$HOME/Videos}/screenrecords"
 
-do_record() {
-  local file="$srDir/screenrecording-$(date +'%Y-%m-%d_%H-%M-%S').mp4"
-  local region=$(slurp -o)
-
-  [[ -z "$region" ]] && { notify-send -e "Screen recording cancelled" -i "replay-record-error" -t 3000; exit 1; }
-  
+start_record() {
+  local file="$scr_dir/screenrecording-$(date +'%Y-%m-%d_%H-%M-%S').mp4"
   if lspci | grep -qi 'nvidia'; then
-    wf-recorder --audio=$(pactl get-default-sink).monitor -g "$region" -f "$file" -c libx264 -p crf=23 -p preset=medium -p movflags=+faststart &
+    wf-recorder --audio="$(pactl get-default-sink).monitor" -g "$region" -f "$file" -c libx264 -p crf=23 -p preset=medium -p movflags=+faststart &
   else
-    wf-recorder --audio=$(pactl get-default-sink).monitor -g "$region" -f "$file" --ffmpeg-encoder-options="-c:v libx264 -crf 23 -preset medium -movflags +faststart" &
+    wf-recorder --audio="$(pactl get-default-sink).monitor" -g "$region" -f "$file" --ffmpeg-encoder-options="-c:v libx264 -crf 23 -preset medium -movflags +faststart" &
   fi
-}
-
-stop_record() {
-  pkill -x wf-recorder
 }
 
 do_deps() {
   local deps=(wf-recorder slurp)
-
   for dep in "${deps[@]}"; do
     if ! command -v "$dep" >/dev/null 2>&1; then
       echo -e "${error}Error${reset}: Missing dependency ${bold}$dep"
@@ -34,14 +25,15 @@ do_deps() {
   done
 }
 
-mkdir -p "$srDir"
+main() {
+  mkdir -p "$scr_dir"
+  do_deps
+  if pgrep -x wf-recorder >/dev/null; then
+    pkill -x wf-recorder && notify-send -e "Screen recording saved to $scr_dir" -i "screencast-recorded-symbolic" -t 3000
+  else
+    region=$(slurp -o 2>/dev/null) || { notify-send -e "Screen recording cancelled" -i "replay-record-error" -t 3000; exit 1; }
+    start_record && notify-send -e "Screen recording started" -i "replay-record" -t 3000
+  fi
+}
 
-do_deps
-
-if pgrep -x wf-recorder >/dev/null; then
-  stop_record
-  notify-send -e "Screen recording saved to $srDir" -i "screencast-recorded-symbolic" -t 3000
-else
-  do_record
-  notify-send -e "Screen recording started" -i "replay-record" -t 3000
-fi
+main
